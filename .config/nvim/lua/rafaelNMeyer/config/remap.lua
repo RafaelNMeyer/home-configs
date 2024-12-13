c6d8e6ba5c74f5c72bc737fe4a473787d8479a63
RafaelNMeyer <rafaelniederauer07@gmail.com>
2024-12-13 15:52:34 -0300

    refactor nvim remap config

--- .config/nvim/lua/rafaelNMeyer/config/remap.lua
+++ .config/nvim/lua/rafaelNMeyer/config/remap.lua
@@ -1,11 +1,6 @@
 -- If the line is broke, go one by one
-vim.keymap.set('n', 'k', 'v:count == 0 ? "gk" : "k"', {expr = true, noremap = true})
-vim.keymap.set('n', 'j', 'v:count == 0 ? "gj" : "j"', {expr = true, noremap = true})
-
-vim.keymap.set('v', 'k', 'v:count == 0 ? "gk" : "k"', {expr = true, noremap = true})
-vim.keymap.set('v', 'j', 'v:count == 0 ? "gj" : "j"', {expr = true, noremap = true})
-
-vim.keymap.set('v', '<Leader>1f', vim.lsp.buf.format)
+vim.keymap.set({'n', 'v'}, 'k', 'v:count == 0 ? "gk" : "k"', {expr = true})
+vim.keymap.set({'n', 'v'}, 'j', 'v:count == 0 ? "gj" : "j"', {expr = true})
 
 -- Move line 
 -- vim.keymap.set('n', '<C-j>', "<cmd>m+1<CR>", {})
