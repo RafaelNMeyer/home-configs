dcaf332d5f4d69decff8d5dd11ca5b4c2e77626c
RafaelNMeyer <rafaelniederauer07@gmail.com>
2024-12-15 18:53:11 -0300

    set highlight on yank auto command in nvim

--- .config/nvim/lua/rafaelNMeyer/config/set.lua
+++ .config/nvim/lua/rafaelNMeyer/config/set.lua
@@ -17,3 +17,7 @@ opt.termguicolors = true
 opt.clipboard = "unnamedplus"
 
 vim.g.mapleader = " "
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+	callback = function() vim.highlight.on_yank() end,
+})
