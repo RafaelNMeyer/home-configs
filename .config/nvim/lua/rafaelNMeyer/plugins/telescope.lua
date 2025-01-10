429cf29c61c1f8260d84e0400f437e1a951805fc
RafaelNMeyer <rafaelniederauer07@gmail.com>
2025-01-10 12:04:52 -0300

    feat(telescope): update find_file behavior based on .git repository presence

--- .config/nvim/lua/rafaelNMeyer/plugins/telescope.lua
+++ .config/nvim/lua/rafaelNMeyer/plugins/telescope.lua
@@ -1,12 +1,26 @@
 return {
-	'nvim-telescope/telescope.nvim', tag = '0.1.5',
-	dependencies = { 'nvim-lua/plenary.nvim' },
-	config = function()
-		local builtin = require('telescope.builtin')
-		vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-		vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-		vim.keymap.set('n', '<leader>fd', builtin.diagnostics, {})
-		vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-		vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-	end
+  'nvim-telescope/telescope.nvim',
+  tag = '0.1.8',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  require("telescope").setup({
+    defaults = {
+    }
+  }),
+  config = function()
+    local builtin = require('telescope.builtin')
+    vim.keymap.set('n', '<leader>ff', function()
+      local contains_git = (#vim.fs.find(
+        { '.git' }, { path = '.', upward = true }) ~= 0)
+
+      if contains_git then
+        return builtin.git_files()
+      end
+
+      return builtin.find_files({ hidden = true, no_ignore = true })
+    end, {})
+    vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+    vim.keymap.set('n', '<leader>fd', builtin.diagnostics, {})
+    vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+    vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+  end
 }
