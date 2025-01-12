3e17c76435adaa6703d29c55c618ed498915e62b
RafaelNMeyer <rafaelniederauer07@gmail.com>
2025-01-12 20:59:21 -0300

    fix(vim.opt): remove unused options

--- .config/nvim/lua/rafaelNMeyer/config/set.lua
+++ .config/nvim/lua/rafaelNMeyer/config/set.lua
@@ -7,17 +7,12 @@ opt.tabstop = 2
 opt.shiftwidth = 2
 opt.smartindent = true
 opt.scroll = 15
-
---  vim.opt.hlsearch = false
 opt.incsearch = true
-
--- vim.opt.backup = false
-
 opt.termguicolors = true
 opt.clipboard = "unnamedplus"
 
 vim.g.mapleader = " "
 
 vim.api.nvim_create_autocmd("TextYankPost", {
-	callback = function() vim.highlight.on_yank() end,
+  callback = function() vim.highlight.on_yank() end,
 })
