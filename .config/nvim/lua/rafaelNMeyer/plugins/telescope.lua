709cb4a92a3954850425eb22b76ba1d04b7efc18
RafaelNMeyer <rafaelniederauer07@gmail.com>
2025-02-15 20:34:07 -0300

    fix(nvim.telescope): remove early empty telescope.setup config (crash telescope on macos)

--- .config/nvim/lua/rafaelNMeyer/plugins/telescope.lua
+++ .config/nvim/lua/rafaelNMeyer/plugins/telescope.lua
@@ -2,10 +2,6 @@ return {
   'nvim-telescope/telescope.nvim',
   tag = '0.1.8',
   dependencies = { 'nvim-lua/plenary.nvim' },
-  require("telescope").setup({
-    defaults = {
-    }
-  }),
   config = function()
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<leader>ff', function()
