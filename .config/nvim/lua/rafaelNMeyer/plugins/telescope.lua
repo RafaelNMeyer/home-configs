2cacca2d90238493b567df59cc81a30df28f12ea
rnmeyer_meli <rafael.nmeyer@mercadolivre.com>
2025-06-08 23:22:40 -0300

    fix(nvim.telescope): fix macos nvim find files

--- .config/nvim/lua/rafaelNMeyer/plugins/telescope.lua
+++ .config/nvim/lua/rafaelNMeyer/plugins/telescope.lua
@@ -1,22 +1,43 @@
 return {
-  'nvim-telescope/telescope.nvim',
-  tag = '0.1.8',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  config = function()
-    local builtin = require('telescope.builtin')
-    vim.keymap.set('n', '<leader>ff', function()
-      local contains_git = (#vim.fs.find(
-        { '.git' }, { path = '.', upward = true }) ~= 0)
+	'nvim-telescope/telescope.nvim',
+	tag = '0.1.8',
+	dependencies = { 'nvim-lua/plenary.nvim' },
 
-      if contains_git then
-        return builtin.git_files()
-      end
+	-- Clone the default Telescope configuration
+	--
+	config = function()
+		local telescope = require("telescope")
+		local telescopeConfig = require("telescope.config")
 
-      return builtin.find_files({ hidden = true, no_ignore = true })
-    end, {})
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-    vim.keymap.set('n', '<leader>fd', builtin.diagnostics, {})
-    vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-    vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-  end
+		local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+
+		-- I want to search in hidden/dot files.
+		table.insert(vimgrep_arguments, "--hidden")
+		-- I don't want to search in the `.git` directory.
+		table.insert(vimgrep_arguments, "--glob")
+		table.insert(vimgrep_arguments, "!**/.git/*")
+		telescope.setup({
+			defaults = {
+				-- `hidden = true` is not supported in text grep commands.
+				vimgrep_arguments = vimgrep_arguments,
+			},
+			pickers = {
+				find_files = {
+					hidden = true,
+					no_ignore = true,
+					find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+					file_ignore_patterns = {
+						"node_modules",
+						".git",
+					},
+				},
+			},
+		})
+		local builtin = require('telescope.builtin')
+		vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+		vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+		vim.keymap.set('n', '<leader>fd', builtin.diagnostics, {})
+		vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+		vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+	end
 }
